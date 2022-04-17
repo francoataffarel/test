26ffdf06b461ae52456c0430a53d7d12017614f8
avishmehta <avish@mensabrands.com>
2022-04-17 15:52:20 +0530

    test

--- test_suite/file_changes.py
+++ test_suite/file_changes.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     # Fetch Jobs
     #############################################
     if 'ALL_FILES' in changed_files:
-        projects = ['./mensa-scraper/rnr/rnr/spiders/marketplace']
+        projects = ['.github/mensa-scraper/rnr/rnr/spiders/marketplace']
         print("projects",projects,os.system("pwd && ls -al"))
         changed_files = []
         for project in projects:
